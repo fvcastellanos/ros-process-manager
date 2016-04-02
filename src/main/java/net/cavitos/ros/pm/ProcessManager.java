a280f2e372514b281766b261ae25bf306d934a94
fvcastellanos@gmail.com <fvcastellanos@gmail.com>
2016-04-02 13:44:20 -0600

    V1: Listed windows pid and pname

--- src/main/java/net/cavitos/ros/pm/ProcessManager.java
+++ src/main/java/net/cavitos/ros/pm/ProcessManager.java
@@ -4,28 +4,46 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
 import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Tlhelp32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.win32.W32APIOptions;
+
+import java.util.Scanner;
 
 public class ProcessManager {
-    // This is the standard, stable way of mapping, which supports extensive
-    // customization and mapping of Java to native types.
-
-    public interface CLibrary extends Library {
-        CLibrary INSTANCE = (CLibrary)
-                Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"),
-                        CLibrary.class);
-
-        void printf(String format, Object... args);
-    }
 
     public static void main(String[] args) {
-        CLibrary.INSTANCE.printf("Hello, World\n");
-        for (int i=0;i < args.length;i++) {
-            CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
-        }
-
+/*
         Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
         // Optional: wraps every call to the native library in a
         // synchronized block, limiting native calls to one at a time
         Kernel32 SYNC_INSTANCE = (Kernel32) Native.synchronizedLibrary(INSTANCE);
+
+        System.out.println("Current thread: " + SYNC_INSTANCE.GetCurrentThread().toString());
+        System.out.println("Current process: " + SYNC_INSTANCE.GetCurrentProcess().toString());
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextInt();
+*/
+
+        Kernel32 kernel32 = (Kernel32) Native.loadLibrary(Kernel32.class, W32APIOptions.UNICODE_OPTIONS);
+        Tlhelp32.PROCESSENTRY32.ByReference processEntry = new Tlhelp32.PROCESSENTRY32.ByReference();
+
+        WinNT.HANDLE snapshot = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinDef.DWORD(0));
+        try  {
+            while (kernel32.Process32Next(snapshot, processEntry)) {
+                System.out.printf("PID: %d, Name: %s\n", Integer.parseInt(processEntry.th32ProcessID.toString()),
+                        Native.toString(processEntry.szExeFile));
+//                System.out.println(processEntry.th32ProcessID + "\t" + Native.toString(processEntry.szExeFile));
+            }
+        }
+        finally {
+            kernel32.CloseHandle(snapshot);
+        }
     }
+
+
 }
+
